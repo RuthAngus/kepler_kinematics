5ce050b57eea39c266eee56de8d3517ff93f8431
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2021-02-23 15:46:18 -0500

    adding test_data.py

---
+++ code/test_data.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import data as d
+import pandas as pd
+
+
+def test_load_and_merge_data():
+    df = d.load_and_merge_data()
+    return df
+
+def test_combine_rv_measurements(df):
+    rvdf = d.combine_rv_measurements(df)
+
+    plt.plot(rvdf.OBSVHELIO_AVG.values, rvdf.radial_velocity, ".")
+    plt.plot(df.OBSVHELIO_AVG.values, rvdf.rv, ".")
+    plt.savefig("test")
+    plt.close()
+
+    return rvdf
+
+if __name__ == "__main__":
+    df = test_load_and_merge_data()
+    rvdf = test_combine_rv_measurements(df)
+
+    si = [f"{i:f}" for i in rvdf.source_id.values]
+    names = pd.DataFrame(dict({"source_id": si}))
+    names.to_csv("source_ids.csv", index=False)
